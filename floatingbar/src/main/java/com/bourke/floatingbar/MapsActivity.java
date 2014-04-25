b435ed4d880fd6b87e9c3f01d4fcdf15940aea07
Paul Bourke <pauldbourke@gmail.com>
2014-04-26 00:43:23 +0100

    Update bar based on distance to target
    Progress bar now reflects distance to target via progress and color.
    
    Also:
    
    * Add start/stop menu item
    * Add location tracking to MainActivity to show current postion on
      map
    * Add location tracking to ProgressBarService to update bar based on
      distance to target

--- floatingbar/src/main/java/com/bourke/floatingbar/MapsActivity.java
+++ floatingbar/src/main/java/com/bourke/floatingbar/MapsActivity.java
@@ -1,26 +1,55 @@
 package com.bourke.floatingbar;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements
         GooglePlayServicesClient.ConnectionCallbacks,
-        GooglePlayServicesClient.OnConnectionFailedListener {
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final String TAG = "FloatingBar/MainActivity";
+
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL = 3000;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL = 1000;
+
+    /*
+     * Define a request code to send to Google Play services
+     * This code is returned in Activity.onActivityResult
+     */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     private LocationClient mLocationClient;
+    private LocationRequest mLocationRequest;
+
+    private Marker now;
+
+    private MenuItem mMenuStart;
+    private MenuItem mMenuStop;
 
     @Override public void onConnected(Bundle bundle) {
         // Global variable to hold the current location
@@ -31,22 +60,87 @@ public class MapsActivity extends FragmentActivity implements
                 .zoom(17)                   // Sets the zoom
                 .build();                   // Creates a CameraPosition from the builder
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        // TODO: they recommend wrapping a boolean here to disable updates if user requests
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
     }
 
     @Override public void onDisconnected() {
-
+        Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
     }
 
     @Override public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(
+                        this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                * Thrown if Google Play services canceled the original
+                * PendingIntent
+                */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Toast.makeText(this, "Connection Failure", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    @Override public void onLocationChanged(Location location) {
+        if(now != null){
+            now.remove();
+        }
+
+        // Getting latitude of the current location
+        double latitude = location.getLatitude();
+
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+
+        // Creating a LatLng object for the current location
+        LatLng latLng = new LatLng(latitude, longitude);
+        now = mMap.addMarker(new MarkerOptions().position(latLng));
+        // Showing the current location in Google Map
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        // Zoom in the Google Map
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
         mLocationClient = new LocationClient(this, this, this);
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // TODO: check if play services available:
+        // https://developer.android.com/training/location/receive-location-updates.html
     }
 
     @Override
@@ -70,9 +164,47 @@ public class MapsActivity extends FragmentActivity implements
      */
     @Override
     protected void onStop() {
+        if (mLocationClient.isConnected()) {
+            mLocationClient.removeLocationUpdates(this);
+        }
+
         // Disconnecting the client invalidates it.
         mLocationClient.disconnect();
+
         super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+
+        mMenuStart = menu.findItem(R.id.action_start);
+        mMenuStop = menu.findItem(R.id.action_stop);
+        mMenuStop.setVisible(false);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_start:
+                // TODO: check if maps are ready
+                startService(new Intent(this, ProgressBarService.class));
+                mMenuStart.setVisible(false);
+                mMenuStop.setVisible(true);
+                break;
+
+            case R.id.action_stop:
+                stopService(new Intent(this, ProgressBarService.class));
+                mMenuStop.setVisible(false);
+                mMenuStart.setVisible(true);
+                break;
+        }
+
+        return super.onMenuItemSelected(featureId, item);
     }
 
     /**
--- floatingbar/src/main/java/com/bourke/floatingbar/ProgressBarService.java
+++ floatingbar/src/main/java/com/bourke/floatingbar/ProgressBarService.java
@@ -5,12 +5,32 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
+import android.location.Location;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class ProgressBarService extends Service {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+
+public class ProgressBarService extends Service implements
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener,
+        LocationListener {
+
+    private static final String TAG = "FloatingBar/ProgressBarService";
+
+    // Update frequency in milliseconds
+    private static final long UPDATE_INTERVAL = 3000;
+    // A fast frequency ceiling in milliseconds
+    private static final long FASTEST_INTERVAL = 1000;
 
     private WindowManager windowManager;
     private ProgressBar mProgressBar;
@@ -19,6 +39,13 @@ public class ProgressBarService extends Service {
 
     private final int HOLO_GREEN = Color.rgb(153, 204, 0);
 
+    private LocationClient mLocationClient;
+    private LocationRequest mLocationRequest;
+
+    private Location mStartingPoint = new Location(TAG);
+    private Location mDestination = new Location(TAG);
+    private float mTotalDistance;
+
     @Override public IBinder onBind(Intent intent) {
         // Not used
         return null;
@@ -26,6 +53,26 @@ public class ProgressBarService extends Service {
 
     @Override public void onCreate() {
         super.onCreate();
+
+        mLocationClient = new LocationClient(this, this, this);
+        mLocationClient.connect();
+
+        // TODO: set from UI
+        mStartingPoint.setLatitude(53.345905);
+        mStartingPoint.setLongitude(-6.294289);
+        mDestination.setLatitude(53.347799);
+        mDestination.setLongitude(-6.243922);
+        mTotalDistance = mStartingPoint.distanceTo(mDestination);
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create();
+        // Use high accuracy
+        mLocationRequest.setPriority(
+                LocationRequest.PRIORITY_HIGH_ACCURACY);
+        // Set the update interval to 5 seconds
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        // Set the fastest update interval to 1 second
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
 
         windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
 
@@ -45,13 +92,56 @@ public class ProgressBarService extends Service {
         params.x = 0;
         params.y = 0;
 
-        mProgressBar.setProgress(100);
+        mProgressBar.setProgress(mProgressStatus);
 
         windowManager.addView(mProgressBar, params);
     }
 
     @Override public void onDestroy() {
         super.onDestroy();
+
+        if (mLocationClient.isConnected()) {
+            mLocationClient.removeLocationUpdates(this);
+        }
+
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+
         if (mProgressBar != null) windowManager.removeView(mProgressBar);
+    }
+
+    @Override public void onConnected(Bundle bundle) {
+        // TODO: they recommend wrapping a boolean here to disable updates if user requests
+        mLocationClient.requestLocationUpdates(mLocationRequest, this);
+    }
+
+    @Override public void onDisconnected() {
+        Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(this, "Connection Failure", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override public void onLocationChanged(Location location) {
+        Location currentLocation = mLocationClient.getLastLocation();
+        float distanceRemaining = currentLocation.distanceTo(mDestination);
+
+        mProgressStatus = (int) ((distanceRemaining / mTotalDistance) * 100);
+        mProgressStatus = Math.abs(mProgressStatus - 100);
+
+        mProgressBar.setProgress(mProgressStatus);
+
+        if (mProgressStatus >= 80) {
+            mProgressBar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        } else if (mProgressStatus >= 60) {
+            mProgressBar.getProgressDrawable().setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_IN);
+        } else {
+            mProgressBar.getProgressDrawable().setColorFilter(HOLO_GREEN, PorterDuff.Mode.SRC_IN);
+        }
+
+        Log.d(TAG, location.toString());
+        Log.d(TAG, "Distance remaining (meters): " + distanceRemaining);
+        Log.d(TAG, "Distance complete (percentage): " + mProgressStatus);
     }
 }
